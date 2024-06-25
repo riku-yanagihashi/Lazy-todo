8a7a5802d6ac72b3f6d21942aec41d599de46391
柳橋莉空 <yagihashirikuu@yagihashirikuunoMacBook-Air.local>
2024-06-26 02:47:20 +0900

    feat: Changed to make it easier to set priority :+1:

--- todo_cli/src/main.rs
+++ todo_cli/src/main.rs
@@ -66,6 +66,47 @@ enum InputMode {
     EditingDeadline(usize),
 }
 
+#[derive(PartialEq, Copy, Clone)]
+enum PrioritySelection {
+    Low,
+    Medium,
+    High,
+}
+
+impl PrioritySelection {
+    fn next(&self) -> Self {
+        match *self {
+            PrioritySelection::Low => PrioritySelection::Medium,
+            PrioritySelection::Medium => PrioritySelection::High,
+            PrioritySelection::High => PrioritySelection::Low,
+        }
+    }
+
+    fn prev(&self) -> Self {
+        match *self {
+            PrioritySelection::Low => PrioritySelection::High,
+            PrioritySelection::Medium => PrioritySelection::Low,
+            PrioritySelection::High => PrioritySelection::Medium,
+        }
+    }
+
+    fn to_str(&self) -> &'static str {
+        match *self {
+            PrioritySelection::Low => "low",
+            PrioritySelection::Medium => "medium",
+            PrioritySelection::High => "high",
+        }
+    }
+
+    fn color(&self) -> Color {
+        match *self {
+            PrioritySelection::Low => Color::Green,
+            PrioritySelection::Medium => Color::Yellow,
+            PrioritySelection::High => Color::Red,
+        }
+    }
+}
+
 fn main() -> Result<(), io::Error> {
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -80,7 +121,7 @@ fn main() -> Result<(), io::Error> {
     let mut input_mode = InputMode::Normal;
     let mut input_title = String::new();
     let mut input_content = String::new();
-    let mut input_priority = String::new();
+    let mut input_priority = PrioritySelection::Low;
     let mut input_deadline = String::new();
 
     loop {
@@ -93,7 +134,7 @@ fn main() -> Result<(), io::Error> {
                     [
                         Constraint::Length(3),
                         Constraint::Min(1),
-                        Constraint::Length(3),
+                        Constraint::Length(6), // 調整された部分
                     ]
                     .as_ref(),
                 )
@@ -138,19 +179,36 @@ fn main() -> Result<(), io::Error> {
                 .highlight_style(Style::default().bg(Color::Blue));
             f.render_stateful_widget(todos_list, chunks[1], &mut state);
 
+            let instructions_chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Min(1), Constraint::Length(3)].as_ref())
+                .split(chunks[2]);
+
             let instructions = match input_mode {
                 InputMode::Normal => {
                     String::from("q: Quit | a: Add | d: Delete | e: Edit | j: Down | k: Up | Enter: Toggle Done")
                 }
                 InputMode::AddingTitle | InputMode::EditingTitle(_) => format!("Enter title: {}", input_title),
                 InputMode::AddingContent | InputMode::EditingContent(_) => format!("Enter content: {}", input_content),
-                InputMode::AddingPriority | InputMode::EditingPriority(_) => format!("Enter priority: {}", input_priority),
+                InputMode::AddingPriority | InputMode::EditingPriority(_) => {
+                    let priority_symbol = match input_priority {
+                        PrioritySelection::Low => "●",
+                        PrioritySelection::Medium => "●",
+                        PrioritySelection::High => "●",
+                    };
+                    let priority_color = input_priority.color();
+                    let priorities_list = Paragraph::new(Spans::from(vec![Span::styled(priority_symbol, Style::default().fg(priority_color))]))
+                        .block(Block::default().borders(Borders::ALL).title("Select priority"));
+
+                    f.render_widget(priorities_list, instructions_chunks[1]);
+                    format!("Use j/k to change priority")
+                },
                 InputMode::AddingDeadline | InputMode::EditingDeadline(_) => format!("Enter deadline: {}", input_deadline),
             };
-            let instructions = Paragraph::new(instructions)
+            let instructions_paragraph = Paragraph::new(instructions)
                 .style(Style::default().fg(Color::White).bg(Color::Black))
                 .block(Block::default().borders(Borders::ALL).title("Instructions"));
-            f.render_widget(instructions, chunks[2]);
+            f.render_widget(instructions_paragraph, instructions_chunks[0]);
         })?;
 
         if let Event::Key(key) = event::read()? {
@@ -183,7 +241,12 @@ fn main() -> Result<(), io::Error> {
                                     input_mode = InputMode::EditingTitle(selected);
                                     input_title = todos[selected].title.clone();
                                     input_content = todos[selected].content.clone();
-                                    input_priority = todos[selected].priority.clone();
+                                    input_priority = match todos[selected].priority.as_str() {
+                                        "low" => PrioritySelection::Low,
+                                        "medium" => PrioritySelection::Medium,
+                                        "high" => PrioritySelection::High,
+                                        _ => PrioritySelection::Low,
+                                    };
                                     input_deadline = todos[selected].deadline.clone();
                                 }
                             }
@@ -215,7 +278,7 @@ fn main() -> Result<(), io::Error> {
                 }
                 InputMode::AddingTitle => match key.code {
                     KeyCode::Enter => {
-                        if !input_title.is_empty() {
+                        if (!input_title.is_empty()) {
                             input_mode = InputMode::AddingContent;
                         }
                     }
@@ -252,30 +315,30 @@ fn main() -> Result<(), io::Error> {
                     KeyCode::Enter => {
                         input_mode = InputMode::AddingDeadline;
                     }
-                    KeyCode::Char(c) => {
-                        input_priority.push(c);
+                    KeyCode::Char('j') => {
+                        input_priority = input_priority.next();
                     }
-                    KeyCode::Backspace => {
-                        input_priority.pop();
+                    KeyCode::Char('k') => {
+                        input_priority = input_priority.prev();
                     }
                     KeyCode::Esc => {
                         input_mode = InputMode::Normal;
                         input_title.clear();
                         input_content.clear();
-                        input_priority.clear();
+                        input_deadline.clear();
                     }
                     _ => {}
                 },
                 InputMode::AddingDeadline => match key.code {
                     KeyCode::Enter => {
                         if !input_title.is_empty() {
-                            let new_todo = Todo::new(input_title.clone(), input_content.clone(), input_priority.clone(), input_deadline.clone());
+                            let priority_str = input_priority.to_str().to_string();
+                            let new_todo = Todo::new(input_title.clone(), input_content.clone(), priority_str, input_deadline.clone());
                             todos.push(new_todo);
                             save_todos(&todos);
                             input_mode = InputMode::Normal;
                             input_title.clear();
                             input_content.clear();
-                            input_priority.clear();
                             input_deadline.clear();
                         }
                     }
@@ -289,7 +352,6 @@ fn main() -> Result<(), io::Error> {
                         input_mode = InputMode::Normal;
                         input_title.clear();
                         input_content.clear();
-                        input_priority.clear();
                         input_deadline.clear();
                     }
                     _ => {}
@@ -311,7 +373,6 @@ fn main() -> Result<(), io::Error> {
                         input_mode = InputMode::Normal;
                         input_title.clear();
                         input_content.clear();
-                        input_priority.clear();
                         input_deadline.clear();
                     }
                     _ => {}
@@ -331,40 +392,37 @@ fn main() -> Result<(), io::Error> {
                         input_mode = InputMode::Normal;
                         input_title.clear();
                         input_content.clear();
-                        input_priority.clear();
                         input_deadline.clear();
                     }
                     _ => {}
                 },
                 InputMode::EditingPriority(index) => match key.code {
                     KeyCode::Enter => {
-                        todos[index].priority = input_priority.clone();
                         input_mode = InputMode::EditingDeadline(index);
                     }
-                    KeyCode::Char(c) => {
-                        input_priority.push(c);
+                    KeyCode::Char('j') => {
+                        input_priority = input_priority.next();
                     }
-                    KeyCode::Backspace => {
-                        input_priority.pop();
+                    KeyCode::Char('k') => {
+                        input_priority = input_priority.prev();
                     }
                     KeyCode::Esc => {
                         input_mode = InputMode::Normal;
                         input_title.clear();
                         input_content.clear();
-                        input_priority.clear();
                         input_deadline.clear();
                     }
                     _ => {}
                 },
                 InputMode::EditingDeadline(index) => match key.code {
                     KeyCode::Enter => {
+                        todos[index].priority = input_priority.to_str().to_string();
                         todos[index].deadline = input_deadline.clone();
                         todos[index].date_time = Utc::now().to_rfc3339();
                         save_todos(&todos);
                         input_mode = InputMode::Normal;
                         input_title.clear();
                         input_content.clear();
-                        input_priority.clear();
                         input_deadline.clear();
                     }
                     KeyCode::Char(c) => {
@@ -377,7 +435,6 @@ fn main() -> Result<(), io::Error> {
                         input_mode = InputMode::Normal;
                         input_title.clear();
                         input_content.clear();
-                        input_priority.clear();
                         input_deadline.clear();
                     }
                     _ => {}
